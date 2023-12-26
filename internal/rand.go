6aac40c7f319eb6c4d47b5c9a2a42c0d217dcea2
marnysan111 <o-masato@outlook.jp>
2023-12-26 11:08:30 +0900

    add: rand time.Sleep

--- internal/rand.go
+++ internal/rand.go
@@ -3,10 +3,14 @@ package internal
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 func Rand10Times() {
 	for i := 0; i < 10; i++ {
-		fmt.Println(rand.Intn(10))
+		//rand.Seed(time.Now().UnixNano())
+		randTime := rand.Intn(10)
+		fmt.Println(randTime)
+		time.Sleep(time.Duration(randTime) * time.Second)
 	}
 }
